63e57aa3cc740b808055ac22c7b1e121c4154518
KISHAN SADHU <websitekwebsite@gmail.com>
2026-04-05 18:57:59 +0530

    change the signup page some

--- src/SignUpPage.jsx
+++ src/SignUpPage.jsx
@@ -4,7 +4,6 @@ import { registerUser } from '../services/authService';
 import { supportedLanguages } from '../utils/countryLanguages';
 import { Mail, Lock, ArrowRight, User, Languages, ArrowLeft } from 'lucide-react'
 
-
 export function SignUpPage() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
